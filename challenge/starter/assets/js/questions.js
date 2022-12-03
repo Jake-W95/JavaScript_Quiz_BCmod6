64d7ef57c0e8f85bd3e910d6f8cb8ded3fdd6a2d
PaulKnees <jakewallace368@gmail.com>
2022-12-03 07:07:05 +0000

    Qs tick but skipping #3

--- challenge/starter/assets/js/questions.js
+++ challenge/starter/assets/js/questions.js
@@ -1,3 +1,20 @@
+//Section Elements
+var startScrn = document.getElementById('start-screen');
+var questions = document.getElementById('questions');
+var choices = document.getElementById('choices');
+
+//Button Elements
+var startBtn = document.getElementById('start');
+var aBtn = document.getElementById('ansA');
+var bBtn = document.getElementById('ansB');
+var cBtn = document.getElementById('ansC');
+var dBtn = document.getElementById('ansD');
+
+//Text Elements
+var qTitle = document.getElementById('question-title');
+
+
+//Questions Array
 var questionsList = [{
     question: 'question 1',
     answerA: 'answer A',
@@ -40,12 +57,32 @@ var questionsList = [{
     answerC: 'answer C',
     AnswerD: 'answer D'
 }]
-var startScrn = document.getElementById('start-screen')
-var startBtn = document.getElementById('start')
-var questions = document.getElementById('questions')
 
 
-startBtn = addEventListener('click', function(){
-startScrn.setAttribute('style', 'display:none;');
-questions.setAttribute('style', 'display:block;')
+//Misc
+var count = 0 //Question counter
+
+
+startBtn = addEventListener('click', function () {
+    //Game start
+    startScrn.setAttribute('style', 'display:none;');
+    questions.setAttribute('style', 'display:block;')
+loadQ()
+function loadQ(){
+    var Q = questionsList[count]
+    qTitle.innerHTML = Q.question;
+    aBtn.innerHTML = Q.answerA;
+    bBtn.innerHTML = Q.answerB;
+    cBtn.innerHTML = Q.answerC;
+    dBtn.innerHTML = Q.AnswerD;
+
+    choices.addEventListener('click', function(){
+        count++;
+    })
+}
+
+
+
+
+
 })
