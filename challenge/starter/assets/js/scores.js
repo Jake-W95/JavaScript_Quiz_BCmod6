b8e919099f55b18f57b0fd287f92a5c312dda5fc
PaulKnees <jakewallace368@gmail.com>
2022-12-06 12:42:35 +0000

    reworked LIs, edited script accordingly

--- challenge/starter/assets/js/scores.js
+++ challenge/starter/assets/js/scores.js
@@ -2,8 +2,9 @@ var scoreboard = document.getElementsByClassName('place');
 // var place = scoreboard
 var userScore = localStorage.getItem('userScore');
 var saveBoard;
-// var loadBoard; JSON.parse(localStorage.getItem('board'))
-// var first = document.getElementById('1');
+
+
+
 var userName = ''
 
 // console.log(place, 'place')
@@ -15,21 +16,31 @@ function getUserName() {
     }
 }
 getUserName()
-var nameAndScore = `${userName}:  ${userScore}`
+
+// var nameAndScore = `${userName}:  ${userScore}`
+
 
 
 console.log(scoreboard, 'scoreboard')
 for (i in scoreboard){
-    console.log(scoreboard[i], 'scoreboardIndex')
+    // console.log(scoreboard[i], 'scoreboardIndex')
+var placeName = scoreboard[i].children[0];
+var placeScore = scoreboard[i].children[1];
+var placeScoreValue = JSON.parse(placeScore.innerHTML)
+console.log(placeScoreValue, 'value')
+// var placeScoreValue = JSON.parse(placeScore)
+console.log(placeName, 'pName')
 
+    // var placeScore = JSON.parse(scoreboard[i].innerHTML)
 
-    var placeScore = JSON.parse(scoreboard[i].innerHTML)
-
-    if (userScore > placeScore){
-        scoreboard[i].innerHTML = nameAndScore;
+    if (userScore > placeScoreValue){
+        // scoreboard[i].innerHTML = nameAndScore;
+        console.log(placeName, 'pName')
+        placeName.innerHTML = userName;
+        placeScore.innerHTML = userScore;
         break
     }
-    console.log(placeScore, 'PlaceScore')
+    // console.log(placeScore, 'PlaceScore')
 }
 
 console.log(
